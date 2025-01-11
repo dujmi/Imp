df2d5f2469168cd8c7406d05b70cd268d29ce54f
dujmi <dujakmislav@gmail.com>
2025-01-11 23:58:49 +0100

    Added odds fetching

--- R/load_season_odds.R
+++ R/load_season_odds.R
@@ -1,3 +1,20 @@
-load_season_odds <- function() {
+load_season_odds <- function(id, name) {
+    season <- 24
+
+    odds <- tidytable::tidytable(
+        readr::read_csv(
+            sprintf(
+                "https://www.football-data.co.uk/mmz4281/%d%d/%s.csv",
+                season, season + 1, id
+            )
+        )
+    ) |>
+        tidytable::select(Date, Time, HomeTeam, AwayTeam, AvgCH, AvgCD, AvgCA) |>
+        tidytable::mutate(
+            comp = name,
+            season = season
+        ) |>
+        janitor::clean_names()
+
     return (odds)
 }
