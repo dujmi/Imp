649dfd21106a9787439c2580d134204beaa2d65b
dujmi <dujakmislav@gmail.com>
2025-01-12 21:48:23 +0100

    Added team per game probabilities

--- R/model.R
+++ R/model.R
@@ -5,7 +5,7 @@ source("R/get_standings_distribution.R")
 for (league in leagues) {
     odds <- load_season_odds(league$id, league$name)
 
-    odds <- odds |>
+    probs_grouped <- odds |>
         tidytable::mutate(
             h_vig = 1 / avg_ch,
             d_vig = 1 / avg_cd,
@@ -16,4 +16,22 @@ for (league in leagues) {
             a_nv = a_vig / total_vig
         ) |>
         tidytable::select(datetime, comp, season, home_team, away_team, h_nv, d_nv, a_nv)
+
+    home_probs <- probs_grouped |>
+        tidytable::rename(
+            team = home_team,
+            win = h_nv,
+            draw = d_nv,
+            loss = a_nv
+        ) |>
+        tidytable::select(-away_team)
+    away_probs <- probs_grouped |>
+        tidytable::rename(
+            team = away_team,
+            win = a_nv,
+            draw = d_nv,
+            loss = h_nv
+        ) |>
+        tidytable::select(-home_team)
+    probs <- tidytable::bind_rows(list(home_probs, away_probs))
 }
